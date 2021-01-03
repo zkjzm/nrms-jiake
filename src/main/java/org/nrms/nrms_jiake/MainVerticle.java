ed1be706ec681346bb44667ce36bb56ff37af6b8
zkjzm <26832684@qq.com>
2021-01-03 22:32:58 +0800

    0.1

---
+++ src/main/java/org/nrms/nrms_jiake/MainVerticle.java
@@ -0,0 +1,26 @@
+package org.nrms.nrms_jiake;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.ext.web.Router;
+
+public class MainVerticle extends AbstractVerticle {
+
+  Router router;
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    router = Router.router(vertx);
+    vertx.createHttpServer().requestHandler(req -> {
+      req.response()
+        .putHeader("content-type", "text/plain")
+        .end("Hello from Vert.x!");
+    }).listen(8888, http -> {
+      if (http.succeeded()) {
+        startPromise.complete();
+        System.out.println("HTTP server started on port 8888");
+      } else {
+        startPromise.fail(http.cause());
+      }
+    });
+  }
+}
